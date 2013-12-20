3bbe5263cf63bce70cad2aec9d3a29a2a6fa3f7e
privaloff <jenyapri@gmail.com>
2013-12-20 11:35:07 +0200

    aaa

--- appl/func.py
+++ appl/func.py
@@ -1,12 +1,39 @@
 from django.db import models
 from core.models import Item
 from appl.models import News
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import Http404
 
 
-def getItemsList(cls,  *attr, qty=None):
-    items = eval(cls).objects.select_related().all()[:qty]
+def getItemsListWithPagination(cls,  *attr,  page=1):
+    '''
+    Method  return List of Item of specific class including Pagination
+    cls = (class name of specific Item (News , Company))
+    attr = (list of item's attributes)
+    page = number of current page
+    '''
+    items = eval(cls).objects.select_related().all()
+    paginator = Paginator(items, 2)
+    try:
+      page = items = paginator.page(page)
+    except Exception:
+      page = items = paginator.page(1)
     itemsList = {}
     for item in items:
         itemsList[item.name] = item.getAttributesValue(*attr)
-    return itemsList
 
+    return itemsList, page                                         #Return List Item and Page object of current page
+
+def getItemsList(cls,  *attr,  qny=None):
+    '''
+    Method return List of Item of specific class
+    cls = (class name of specific Item (News , Company))
+    attr = (list of item's attributes)
+    qny = (number of returned items)
+    '''
+    items = eval(cls).objects.select_related().all()[:None]
+    itemsList = {}
+    for item in items:
+        itemsList[item.name] = item.getAttributesValue(*attr)
+
+    return itemsList
--- appl/views.py
+++ appl/views.py
@@ -1,11 +1,13 @@
+from itertools import count
 from django.shortcuts import render_to_response
 from appl.models import News
 from core.models import Value, Item
 from appl import func
 
-# Create your views here.
-
 
 def set_news_list(request):
-    itemsList = func.getItemsList("News", "Detail Text", "Detail Picture")
-    return render_to_response("NewsList.html",locals())
+    page = request.GET.get('page',1)
+    result = func.getItemsListWithPagination("News", "Detail Text", "Detail Picture", page=page)
+    itemsList = result[0]
+    page = result[1]
+    return render_to_response('NewsList.html',locals())
