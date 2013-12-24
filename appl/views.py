9cebebde949fcf39ec86ac9c2c1901ca2e748c30
privaloff <jenyapri@gmail.com>
2013-12-24 15:33:09 +0200

    new methods core

--- appl/views.py
+++ appl/views.py
@@ -1,20 +1,24 @@
 from itertools import count
 from django.shortcuts import render_to_response
 from appl.models import News
-from core.models import Value, Item, Attribute
+from core.models import Value, Item, Attribute ,Dictionary
 from appl import func
 
 
 def set_news_list(request):
+    #dict = Dictionary.objects.get(title="Sex")
+    #dict.deleteSlot("Jopa")
 
 
-    new = News(name='new', title='new')
-    new.save()
-    new.creatAndSetAttribute('Anonce Text', 'str')
+
+
+
+
 
 
     page = request.GET.get('page', 1)
-    result = func.getItemsListWithPagination("News", "Detail Text", "Detail Picture", page=page)
+    result = func.getItemsListWithPagination("News", "Detail_Text", "Detail_Picture","Anonce_Text", page=page)
+
     itemsList = result[0]
     page = result[1]
     return render_to_response('NewsList.html',locals())
