521d782fa6410be0de0442b727147a5800eab50d
afend69 <afend69@gmail.com>
2013-12-26 10:56:07 +0200

    Extended User class by Client class

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,26 @@
-from core.models import (Action, ActionPath, Attribute, Identity, Item, Participant, Permission, Relationship,
+from core.models import (Action, ActionPath, Attribute, Client, Item, Relationship,
                          Dictionary, State, Slot, Process, Value)
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class ClientInline(admin.StackedInline):
+    model = Client
+    can_delete = False
+    verbose_name_plural = 'client'
+
+class UserAdmin(UserAdmin):
+    inline = (ClientInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Action)
 admin.site.register(ActionPath)
 admin.site.register(Attribute)
-admin.site.register(Identity)
+admin.site.register(Client)
 admin.site.register(Item)
-admin.site.register(Participant)
 admin.site.register(Process)
-admin.site.register(Permission)
 admin.site.register(Relationship)
 admin.site.register(Dictionary)
 admin.site.register(State)
