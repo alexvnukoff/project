dace26c98ac02ec84a39b6e15d147b9bd0b639ae
afend69 <afend69@gmail.com>
2014-01-15 16:21:40 +0200

    Update getItemInstPermList - returns permissions list only for current object type

--- appl/models.py
+++ appl/models.py
@@ -97,6 +97,10 @@ class Department(Organization):
             ("read_department", "Can read department"),
         )
 
+    def __init__(self, *args, **kwargs):
+        super(Department, self).__init__(*args, **kwargs)
+        self.status = State.objects.get(title='Default Department State')
+
     def __str__(self):
         return self.name
 
@@ -310,10 +314,21 @@ def departmentCommunity(instance, **kwargs):
 #----------------------------------------------------------------------------------------------------------
 #Default Groups with Permissions
 read_item = Permission.objects.get(codename='read_item')
+
 read_tpp = Permission.objects.get(codename='read_tpp')
+add_tpp = Permission.objects.get(codename='add_tpp')
+change_tpp = Permission.objects.get(codename='change_tpp')
+delete_tpp = Permission.objects.get(codename='delete_tpp')
+
 read_company = Permission.objects.get(codename='read_company')
 add_company = Permission.objects.get(codename='add_company')
+change_company = Permission.objects.get(codename='change_company')
+delete_company = Permission.objects.get(codename='delete_company')
+
 read_department = Permission.objects.get(codename='read_department')
+add_department = Permission.objects.get(codename='add_department')
+change_department = Permission.objects.get(codename='change_department')
+delete_department = Permission.objects.get(codename='delete_department')
 
 gr1, created = Group.objects.get_or_create(name='Default TPP Permissions')
 if created:
@@ -333,10 +348,19 @@ if created:
 
 gr5, created = Group.objects.get_or_create(name='Owner')
 if created:
-    gr5.permissions.add(read_company, add_company)
+    gr5.permissions.add(read_company, change_company, delete_company,
+                        read_department, change_department, delete_department)
 
+gr6, created = Group.objects.get_or_create(name='Admin')
+if created:
+    gr6.permissions.add(read_company, change_company,
+                        read_department, change_department)
+
+gr7, created = Group.objects.get_or_create(name='Staff')
+if created:
+    gr7.permissions.add(read_company, read_department)
 
 #Default States
 st1, created=State.objects.get_or_create(title='Default TPP State', perm=gr1)
 st2, created=State.objects.get_or_create(title='Default Company State', perm=gr2)
-st3, created=State.objects.get_or_create(title='Default Department State', perm=gr3)
+st3, created=State.objects.get_or_create(title='Default Department State')
