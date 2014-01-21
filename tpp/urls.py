d3e2935dfc9337087ccadef142e72356eaf28db0
afend69 <afend69@gmail.com>
2014-01-21 14:02:45 +0200

    Update user registration - 3

--- tpp/urls.py
+++ tpp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
 import appl.views
 
 from django.contrib import admin
@@ -12,6 +13,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^news/$', appl.views.set_news_list),
+
+    url(r'^accounts/password/change/$', auth_views.password_change, name='password_change'),
+    url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^accounts/password/reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^accounts/password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/profile/', appl.views.cabinet),
 )
