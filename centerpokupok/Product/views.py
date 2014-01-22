1fcf26399c5cac7782aba351d3e4ca8dd674413b
privaloff <jenyapri@gmail.com>
2014-01-22 19:44:22 +0200

    new pages for b2c

--- centerpokupok/Product/views.py
+++ centerpokupok/Product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response, get_object_or_404
-from appl.models import News, Product, Comment, Category
+from appl.models import News, Product, Comment, Category, Company, Country
 from core.models import Value, Item, Attribute, Dictionary, Relationship
 from appl import func
 from django.http import Http404
@@ -9,7 +9,7 @@ from django.core.urlresolvers import reverse
 from tppcenter.forms import ItemForm
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-
+from django.db.models import Q
 from collections import OrderedDict
 from django.utils.translation import ugettext as _
 from django.conf import settings
@@ -79,6 +79,54 @@ def addComment(request, item_id):
 
         else:
             return form
+
+
+
+def getCategoryProduct(request, category_id):
+
+    page = request.GET.get("page", 1)
+
+    products = Product.objects.filter(c2p__parent_id=category_id, c2p__type='rel', sites=settings.SITE_ID).order_by("-pk")
+    result = func.setPaginationForItemsWithValues(products, "NAME", 'DETAIL_TEXT', 'IMAGE', 'COST', 'CURRENCY',
+                                         page_num=12, page=1)
+    products_list = result[0]
+    products_ids = [key for key, value in products_list.items()]
+    companies = Company.objects.filter(p2c__child_id__in=products_ids)
+    items = Item.objects.filter(p2c__child_id__in=companies, p2c__type="rel", pk__in=Country.objects.all(),
+                                 p2c__child__p2c__child__in=products_ids).values("country", "p2c__child_id",
+                                                                                 'p2c__child__p2c__child', 'pk')
+    items_id =[]
+    for item in items:
+        items_id.append(item['pk'])
+        items_id.append(item['p2c__child_id'])
+
+    items_id = set(items_id)
+    itemsWithAttribute = Item.getItemsAttributesValues(("NAME", "IMAGE"), items_id)
+    companyList = {}
+    for item in items:
+        products_list[item['p2c__child__p2c__child']].update({'COMPANY_NAME': itemsWithAttribute[item['p2c__child_id']]['NAME']})
+        products_list[item['p2c__child__p2c__child']].update({'COMPANY_IMAGE': itemsWithAttribute[item['p2c__child_id']]['IMAGE']})
+        products_list[item['p2c__child__p2c__child']].update({'COMPANY_ID': item['p2c__child_id']})
+        products_list[item['p2c__child__p2c__child']].update({'COUNTRY_NAME': itemsWithAttribute[item['pk']]['NAME']})
+        products_list[item['p2c__child__p2c__child']].update({'COUNTRY_ID': item['pk']})
+        if item["p2c__child_id"] not in companyList:
+            companyList[item["p2c__child_id"]] = {}
+            companyList[item["p2c__child_id"]].update({"COMPANY_NAME": itemsWithAttribute[item['p2c__child_id']]['NAME']})
+            companyList[item["p2c__child_id"]].update({"COMPANY_IMAGE": itemsWithAttribute[item['p2c__child_id']]['IMAGE']})
+            companyList[item["p2c__child_id"]].update({"COUNTRY_NAME": itemsWithAttribute[item['pk']]['NAME']})
+            companyList[item['p2c__child_id']].update({'COUNTRY_ID': item['pk']})
+
+
+
+    page = result[1]
+    paginator_range = func.getPaginatorRange(page)
+
+
+
+
+    return render_to_response("Product/index.html", {'products_list': products_list,'companyList': companyList,
+                                                      'page': page, 'paginator_range': paginator_range})
+
 
 
 
