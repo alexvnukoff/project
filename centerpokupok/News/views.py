daa7e4d8b3c160cc8d5dd4339cdf09ac614eec7a
privaloff <jenyapri@gmail.com>
2014-01-24 13:52:53 +0200

    new b2c pages

--- centerpokupok/News/views.py
+++ centerpokupok/News/views.py
@@ -5,16 +5,18 @@ from core.models import Value, Item, Attribute, Dictionary
 from appl import func
 from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 
 from django.conf import settings
 
-def newsList(request):
-    page = request.GET.get('page', 1)
+def newsList(request, page=1):
+    page = page
     result = func.getItemsListWithPagination("News", "NAME", "Active_From", "DETAIL_TEXT", "IMAGE", page=page)
     newsList = result[0]
     page = result[1]
     paginator_range = func.getPaginatorRange(page)
     flagList = func.getItemsList("Country", "NAME", "FLAG")
+    url_paginator = "news:paginator"
 
     return render_to_response("News/index.html", locals())
 
