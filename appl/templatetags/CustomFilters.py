08b2f58aae62c405c6a8e4c68157414a389c47e7
privaloff <jenyapri@gmail.com>
2014-01-24 13:52:03 +0200

    new url filter

--- appl/templatetags/CustomFilters.py
+++ appl/templatetags/CustomFilters.py
@@ -1,6 +1,9 @@
 from django import template
 from collections import OrderedDict
+from django.core.urlresolvers import reverse
+from copy import copy
 register = template.Library()
+
 
 
 @register.filter(name='sort')
@@ -10,5 +13,32 @@ def sort(value):
           return sorted_dict.items()
     else:
         return ""
+
+
+class DynUrlNode(template.Node):
+    def __init__(self, *args):
+        self.name_var = args[0]
+        self.parametrs = args[1]
+        self.new_parametr = args[2]
+
+    def render(self, context):
+        name = template.Variable(self.name_var).resolve(context)
+
+        try:
+            parametrs = copy(template.Variable(self.parametrs).resolve(context))
+        except Exception:
+            parametrs = []
+
+
+        new_parametr = template.Variable(self.new_parametr).resolve(context)
+        parametrs.append(new_parametr)
+
+
+        return reverse(name, args=parametrs)
+
+@register.tag
+def dynurl(parser, token):
+    args = token.split_contents()
+    return DynUrlNode(*args[1:])
 
 
