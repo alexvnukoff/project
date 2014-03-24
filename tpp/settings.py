3eb6715bc313a1f9fa254fb6ff73ab3e8f85185a
root <root@ip-172-31-21-185.eu-west-1.compute.internal>
2014-03-24 15:34:40 +0000

    production fixes

--- tpp/settings.py
+++ tpp/settings.py
@@ -47,8 +47,9 @@ TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [
     '.tppcenter.com', # Allow domain and subdomains
     '.centerpokupok.ru', # Also allow FQDN and subdomains
-    'BC-CIS.COM', # Also allow FQDN and subdomains
-    'B24ONLINE.COM', # Also allow FQDN and subdomains
+    '.BC-CIS.COM', # Also allow FQDN and subdomains
+    '.B24ONLINE.COM', # Also allow FQDN and subdomains
+    '.centerpokupok.com'
 ]
 
 # Application definition
