3f1ba1ef081a92f77a9857afc89be59534f63ccc
afend69 <afend69@gmail.com>
2014-04-10 17:11:03 +0300

    ORG migration.

---
+++ appl/management/commands/clear_db.py
@@ -0,0 +1,35 @@
+from django.core.management.base import NoArgsCommand
+from core.models import User
+from appl.models import Department, Cabinet
+from django.contrib.auth.models import Group
+
+class Command(NoArgsCommand):
+
+    def handle_noargs(self, **options):
+        '''
+            Clear all generated data in DB for ORG community migration process debugging
+        '''
+        print('Starting DB cleaning...')
+        dep_lst = Department.objects.exclude(pk__in=(233, 243))
+        for dep in dep_lst:
+            dep.delete()
+
+        cab_lst = Cabinet.objects.exclude(pk__in=(1, 256, 257, 258, 259, 288))
+        for cab in cab_lst:
+            cab.delete()
+
+        g = Group.objects.get(name='ORG-1000000000')
+        g.user_set.add(User.objects.get(pk=26))
+        g.user_set.add(User.objects.get(pk=27))
+
+        g = Group.objects.get(name='ORG-0100000000')
+        g.user_set.add(User.objects.get(pk=21))
+        g.user_set.add(User.objects.get(pk=22))
+        g.user_set.add(User.objects.get(pk=23))
+
+        g = Group.objects.get(name='ORG-2000000000')
+        g.user_set.add(User.objects.get(pk=24))
+        g.user_set.add(User.objects.get(pk=25))
+        g.user_set.add(User.objects.get(pk=26))
+
+        print('DB is cleaned!')
--- appl/management/commands/move_users_from_communities.py
+++ appl/management/commands/move_users_from_communities.py
@@ -18,7 +18,7 @@ class Command(NoArgsCommand):
         comp_lst = Company.objects.all()
         for comp in comp_lst:
             try:
-                g = Group.objects.get(pk=comp.community)
+                g = Group.objects.get(pk=comp.community.pk)
             except:
                 continue
 
@@ -40,14 +40,16 @@ class Command(NoArgsCommand):
                         except:
                             pass
 
-                    # create department 'Администрация' and add through signal vacancy 'Работник(ца)'
-                    try:
-                        dep, flag = Department.objects.get_or_create(title='DEPARTMENT_FOR_COMP_ID:' + str(comp.pk),
-                                                                     create_user=usr)
-                        if flag:
+                    # create department and add through signal vacancy 'Работник(ца)'
+                    if not Department.objects.filter(item2value__attr__title="NAME",
+                                                 item2value__title_ru__icontains='Департамент_ID_',
+                                                 c2p__parent=comp.pk).exists():
+                        try:
+                            dep = Department.objects.create(title='DEPARTMENT_FOR_COMP_ID:' + str(comp.pk),
+                                                                         create_user=usr)
                             #activate russian locale
                             trans_real.activate('ru')
-                            res = dep.setAttributeValue({'NAME': 'Администрация'}, usr)
+                            res = dep.setAttributeValue({'NAME': 'Департамент_ID_'+str(dep.pk)}, usr)
                             #deactivate russian locale
                             trans_real.deactivate()
                             if not res:
@@ -57,37 +59,40 @@ class Command(NoArgsCommand):
                                 Relationship.objects.create(parent=comp, child=dep, type='hierarchy', create_user=usr)
                             except:
                                 print('Can not create Relationship between Department ID' + dep.pk +
-                                      ' and Company ID' + comp.pk)
+                                          ' and Company ID' + comp.pk)
                                 dep.delete()
-                    except Exception as e:
-                        print('Can not create Department for Company ID:' + comp.pk + '. The reason is:' + str(e))
-                        continue
+                                continue
+                            dep.reindexItem()
+                        except Exception as e:
+                            print('Can not create Department for Company ID:' + comp.pk + '. The reason is:' + str(e))
+                            continue
+                    else:
+                        dep = Department.objects.get(item2value__attr__title="NAME",
+                                                 item2value__title_ru__icontains='Департамент_ID_',
+                                                 c2p__parent=comp.pk)
 
                     try:
-                        vac = Vacancy.objects.get(c2p__parent__c2p__parent=comp.pk, p2c__child=None)
-                    except:
-                        try:
-                            vac = Vacancy.objects.create(title='VACANCY_FOR_ORGANIZATION_ID:' + str(dep.pk),
-                                                         create_user=usr)
-                            #activate russian locale
-                            trans_real.activate('ru')
-                            res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
-                            #deactivate russian locale
-                            trans_real.deactivate()
-                            if not res:
-                                vac.delete()
-                                continue
-                            try:
-                                Relationship.objects.create(parent=dep, child=vac, type='hierarchy', create_user=usr)
-                                #add current user to default Vacancy
-                            except Exception as e:
-                                print('Can not create Relationship between Vacancy ID:' + vac.pk +
-                                      'and Department ID:' + dep.pk + '. The reason is:' + str(e))
-                                vac.delete()
-                                continue
-                        except Exception as e:
-                            print('Can not create Vacancy for Department ID:' + dep.pk + '. The reason is:' + str(e))
+                        vac = Vacancy.objects.create(title='VACANCY_FOR_ORG', create_user=usr)
+                        #activate russian locale
+                        trans_real.activate('ru')
+                        res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
+                        #deactivate russian locale
+                        trans_real.deactivate()
+                        if not res:
+                            vac.delete()
                             continue
+
+                        try:
+                            Relationship.objects.create(parent=dep, child=vac, type='hierarchy', create_user=usr)
+                            #add current user to default Vacancy
+                        except Exception as e:
+                            print('Can not create Relationship between Vacancy ID:' + vac.pk +
+                                    'and Department ID:' + dep.pk + '. The reason is:' + str(e))
+                            vac.delete()
+                            continue
+                    except Exception as e:
+                        print('Can not create Vacancy for Department ID:' + dep.pk + '. The reason is:' + str(e))
+                        continue
 
                     if usr.is_manager:
                         is_admin = True
@@ -97,7 +102,8 @@ class Command(NoArgsCommand):
                     try:
                         Relationship.objects.create(parent=vac, child=cab, type='hierarchy',
                                                     is_admin=is_admin, create_user=usr)
-                        #g.user_set.remove(usr)
+                        g.user_set.remove(usr)
+                        count += 1
                     except:
                         vac.delete()
                         continue
@@ -106,7 +112,7 @@ class Command(NoArgsCommand):
             dep_list = Department.objects.filter(c2p__parent=comp.pk)
             for dep in dep_list:
                 if dep.community:
-                    dep_g = Group.objects.get(pk=dep.community)
+                    dep_g = Group.objects.get(pk=dep.community.pk)
                     dep_usr_list = dep_g.user_set.all()
                     for usr in dep_usr_list:
                         cab, res = Cabinet.objects.get_or_create(user=usr, create_user=usr)
@@ -122,35 +128,32 @@ class Command(NoArgsCommand):
                                 pass
 
                         try:
-                            vac = Vacancy.objects.get(c2p__parent=dep.pk, p2c__child=None)
-                        except:
-                            try:
-                                vac = Vacancy.objects.create(title='VACANCY_FOR_ORGANIZATION_ID:' + str(dep.pk),
+                            vac = Vacancy.objects.create(title='VACANCY_FOR_ORGANIZATION_ID:' + str(dep.pk),
                                                              create_user=usr)
-                                #activate russian locale
-                                trans_real.activate('ru')
-                                res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
-                                #deactivate russian locale
-                                trans_real.deactivate()
-                                if not res:
-                                    vac.delete()
-                                    continue
+                            #activate russian locale
+                            trans_real.activate('ru')
+                            res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
+                            #deactivate russian locale
+                            trans_real.deactivate()
+                            if not res:
+                                vac.delete()
+                                continue
 
-                                try:
-                                    Relationship.objects.create(parent=dep, child=vac,
+                            try:
+                                Relationship.objects.create(parent=dep, child=vac,
                                                                 type='hierarchy', create_user=usr)
 
-                                #add current user to default Vacancy
-                                except Exception as e:
-                                    print('Can not create Relationship between Vacancy ID:' + vac.pk +
-                                          'and Department ID:' + dep.pk + '. The reason is:' + str(e))
-                                    vac.delete()
-                                    continue
-
+                            #add current user to default Vacancy
                             except Exception as e:
-                                print('Can not create Vacancy for Department ID:' + dep.pk +
-                                      '. The reason is:' + str(e))
+                                print('Can not create Relationship between Vacancy ID:' + str(vac.pk) +
+                                          'and Department ID:' + str(dep.pk) + '. The reason is:' + str(e))
+                                vac.delete()
                                 continue
+
+                        except Exception as e:
+                            print('Can not create Vacancy for Department ID:' + str(dep.pk) +
+                                      '. The reason is:' + str(e))
+                            continue
 
                         if usr.is_manager:
                             is_admin = True
@@ -160,7 +163,8 @@ class Command(NoArgsCommand):
                         try:
                             Relationship.objects.create(parent=vac, child=cab, type='hierarchy',
                                                         is_admin=is_admin, create_user=usr)
-                        #g.user_set.remove(usr)
+                            g.user_set.remove(usr)
+                            count += 1
                         except:
                             vac.delete()
                             continue
@@ -173,8 +177,161 @@ class Command(NoArgsCommand):
 
         #cycle through TPP
         tpp_lst = Tpp.objects.all()
-        for tpp in tpp_lst:
-            tpp.pk = 1
+        for comp in tpp_lst:
+            try:
+                g = Group.objects.get(pk=comp.community.pk)
+            except:
+                continue
+
+            #get all users from current group
+            usr_list = g.user_set.all()
+
+            #cycle through Users in current TPP's ORG group
+            if len(usr_list):
+                for usr in usr_list:
+                    cab, res = Cabinet.objects.get_or_create(user=usr, create_user=usr)
+                    if res:
+                        try:
+                            cab.setAttributeValue({'USER_FIRST_NAME': usr.first_name, 'USER_MIDDLE_NAME': '',
+                                                    'USER_LAST_NAME': usr.last_name, 'EMAIL': usr.email}, usr)
+                            group = Group.objects.get(name='Company Creator')
+                            usr.is_manager = True
+                            usr.save()
+                            group.user_set.add(usr)
+                        except:
+                            pass
+
+                    # create department and add through signal vacancy 'Работник(ца)'
+                    if not Department.objects.filter(item2value__attr__title="NAME",
+                                                 item2value__title_ru__icontains='Департамент_ID_',
+                                                 c2p__parent=comp.pk).exists():
+                        try:
+                            dep = Department.objects.create(title='DEPARTMENT_FOR_COMP_ID:' + str(comp.pk),
+                                                                         create_user=usr)
+                            #activate russian locale
+                            trans_real.activate('ru')
+                            res = dep.setAttributeValue({'NAME': 'Департамент_ID_'+str(dep.pk)}, usr)
+                            #deactivate russian locale
+                            trans_real.deactivate()
+                            if not res:
+                                dep.delete()
+                                continue
+                            try:
+                                Relationship.objects.create(parent=comp, child=dep, type='hierarchy', create_user=usr)
+                            except:
+                                print('Can not create Relationship between Department ID' + dep.pk +
+                                          ' and Company ID' + comp.pk)
+                                dep.delete()
+                                continue
+                            dep.reindexItem()
+                        except Exception as e:
+                            print('Can not create Department for Company ID:' + comp.pk + '. The reason is:' + str(e))
+                            continue
+                    else:
+                        dep = Department.objects.get(item2value__attr__title="NAME",
+                                                 item2value__title_ru__icontains='Департамент_ID_',
+                                                 c2p__parent=comp.pk)
+
+                    try:
+                        vac = Vacancy.objects.create(title='VACANCY_FOR_ORG', create_user=usr)
+                        #activate russian locale
+                        trans_real.activate('ru')
+                        res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
+                        #deactivate russian locale
+                        trans_real.deactivate()
+                        if not res:
+                            vac.delete()
+                            continue
+
+                        try:
+                            Relationship.objects.create(parent=dep, child=vac, type='hierarchy', create_user=usr)
+                            #add current user to default Vacancy
+                        except Exception as e:
+                            print('Can not create Relationship between Vacancy ID:' + vac.pk +
+                                    'and Department ID:' + dep.pk + '. The reason is:' + str(e))
+                            vac.delete()
+                            continue
+                    except Exception as e:
+                        print('Can not create Vacancy for Department ID:' + dep.pk + '. The reason is:' + str(e))
+                        continue
+
+                    if usr.is_manager:
+                        is_admin = True
+                    else:
+                        is_admin = False
+
+                    try:
+                        Relationship.objects.create(parent=vac, child=cab, type='hierarchy',
+                                                    is_admin=is_admin, create_user=usr)
+                        g.user_set.remove(usr)
+                        count += 1
+                    except:
+                        vac.delete()
+                        continue
+
+            #cycle through TPP Departments' community Group
+            dep_list = Department.objects.filter(c2p__parent=comp.pk)
+            for dep in dep_list:
+                if dep.community:
+                    dep_g = Group.objects.get(pk=dep.community.pk)
+                    dep_usr_list = dep_g.user_set.all()
+                    for usr in dep_usr_list:
+                        cab, res = Cabinet.objects.get_or_create(user=usr, create_user=usr)
+                        if res:
+                            try:
+                                cab.setAttributeValue({'USER_FIRST_NAME': usr.first_name, 'USER_MIDDLE_NAME': '',
+                                                        'USER_LAST_NAME': usr.last_name, 'EMAIL': usr.email}, usr)
+                                group = Group.objects.get(name='Company Creator')
+                                usr.is_manager = True
+                                usr.save()
+                                group.user_set.add(usr)
+                            except:
+                                pass
+
+                        try:
+                            vac = Vacancy.objects.create(title='VACANCY_FOR_ORGANIZATION_ID:' + str(dep.pk),
+                                                             create_user=usr)
+                            #activate russian locale
+                            trans_real.activate('ru')
+                            res = vac.setAttributeValue({'NAME': 'Работник(ца)'}, usr)
+                            #deactivate russian locale
+                            trans_real.deactivate()
+                            if not res:
+                                vac.delete()
+                                continue
+
+                            try:
+                                Relationship.objects.create(parent=dep, child=vac,
+                                                                type='hierarchy', create_user=usr)
+
+                            #add current user to default Vacancy
+                            except Exception as e:
+                                print('Can not create Relationship between Vacancy ID:' + str(vac.pk) +
+                                          'and Department ID:' + str(dep.pk) + '. The reason is:' + str(e))
+                                vac.delete()
+                                continue
+
+                        except Exception as e:
+                            print('Can not create Vacancy for Department ID:' + str(dep.pk) +
+                                      '. The reason is:' + str(e))
+                            continue
+
+                        if usr.is_manager:
+                            is_admin = True
+                        else:
+                            is_admin = False
+
+                        try:
+                            Relationship.objects.create(parent=vac, child=cab, type='hierarchy',
+                                                        is_admin=is_admin, create_user=usr)
+                            g.user_set.remove(usr)
+                            count += 1
+                        except:
+                            vac.delete()
+                            continue
+                # if Department hasn't community
+                else:
+                    continue
 
         print('Done. Quantity of processed users:', count)
         time2 = datetime.datetime.now()
