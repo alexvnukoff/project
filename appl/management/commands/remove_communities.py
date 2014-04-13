4a04431e4e64528f9fd8b7d6a65b9a1b4814941e
afend69 <afend69@gmail.com>
2014-04-13 11:29:51 +0300

    ORG migration - update.

--- appl/management/commands/remove_communities.py
+++ appl/management/commands/remove_communities.py
@@ -1,9 +1,7 @@
 from django.core.management.base import NoArgsCommand
-from core.models import Item, Relationship
-from appl.models import Tpp, Company, Department, Cabinet, Vacancy
+from core.models import Item
 from django.contrib.auth.models import Group
-from django.utils.translation import trans_real
-import datetime
+
 
 class Command(NoArgsCommand):
 
@@ -14,9 +12,9 @@ class Command(NoArgsCommand):
         print('Start Communities removing...')
         g_list = Group.objects.filter(name__icontains='ORG-')
         for g in g_list:
-            #g.delete()
-            a = 1
-
-        item_list = Item.objects.filter(organization__isnull=False)
+            g.delete()
+            
+        itm = Item.objects.all()
+        itm.update(community=None)
 
         print('All Communities were removed!')
