f87a7054926e99f2c6b2d286ce160d06270261b5
privaloff <jenyapri@gmail.com>
2014-04-29 10:36:54 +0300

    user site fix

--- tppcenter/UserSites/urls.py
+++ tppcenter/UserSites/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
      url(r'^add/$', tppcenter.UserSites.views.resumeForm,{'action': 'add'} , name="add"),
      url(r'^update/(?P<item_id>[0-9]+)/$', tppcenter.UserSites.views.resumeForm,{'action': 'update'}, name="update"),
      url(r'^delete/(?P<item_id>[0-9]+)/$', tppcenter.UserSites.views.resumeForm ,{'action': "delete"}, name="delete"),
-     url(r'^(?P<slug>[a-zA-z0-9-]+-(?P<item_id>[0-9]+))\.html$', tppcenter.UserSites.views.get_resume_list, name="detail"),
+
 
 
 
