ff6971f17a5c4cf877a82449893cfeae1c13258b
Artur K <fatal10110@gmail.com>
2014-05-25 21:29:21 +0300

    admin + adv improved

--- appl/models.py
+++ appl/models.py
@@ -783,16 +783,3 @@ def tppCommunity(instance, **kwargs):
             except Exception as e:
                 print('Can not create Vacancy for Department ID:' + str(dep.pk) + '. The reason is:' + str(e))
     '''
-
-from paypal.standard.ipn.signals import payment_was_successful
-
-def show_me_the_money(sender, **kwargs):
-    ipn_obj = sender
-    # You need to check 'payment_status' of the IPN
-
-    if ipn_obj.payment_status == "Completed":
-        # Undertake some action depending upon `ipn_obj`.
-        if ipn_obj.custom == "Upgrade all users!":
-            Users.objects.update(paid=True)
-
-payment_was_successful.connect(show_me_the_money)
