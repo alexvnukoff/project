c29b9eb1560b5936ea79d728aac9d1585e5116e6
afend69 <afend69@gmail.com>
2014-06-02 11:28:40 +0300

    PayPal setup for IPN debugging - update 2.

--- tppcenter/Companies/views.py
+++ tppcenter/Companies/views.py
@@ -228,9 +228,14 @@ def _companiesDetailContent(request, item_id):
                                                      'TELEPHONE_NUMBER', 'FAX', 'EMAIL', 'SITE_NAME', 'ANONS'))
         #check free membership period
         if company.paid_till_date != None:
-            if (company.paid_till_date - datetime.datetime.now().date()).days <= settings.NOTIFICATION_BEFORE_END_DATE:
+            days_till_end = (company.paid_till_date - datetime.datetime.now().date()).days
+            if (days_till_end <= settings.NOTIFICATION_BEFORE_END_DATE and days_till_end > 0):
                 companyValues['SHOW_PAYMENT_BUTTON'] = [True]
-                companyValues['DAYS_BEFORE_END'] = [settings.NOTIFICATION_BEFORE_END_DATE]
+                companyValues['DAYS_BEFORE_END'] = [days_till_end]
+            else:
+                company.end_date = now()
+                companyValues['SHOW_PAYMENT_BUTTON'] = [True]
+                companyValues['DAYS_BEFORE_END'] = [0]
         else:
             companyValues['SHOW_PAYMENT_BUTTON'] = [False]
         #/check free membership period
--- tppcenter/Payments/views.py
+++ tppcenter/Payments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.http import HttpResponse
 
 def getPaymentDetails(request):
     """
@@ -8,9 +8,17 @@ def getPaymentDetails(request):
         For debugging on local host use: >ngrok.exe -authtoken IV7A72mC4qsjPl5wCVoa -subdomain=tppcenter 80
     """
     payment_details = {}
+    item_number = request.POST.get('item_number', '')
+    if len(item_number):
+        payment_details['ITEM_ID'] = item_number
 
+    payment_details['ITEM_NAME'] = request.POST.get('item_name', '')
+    '''
     templateParams = {
         'payment_details': payment_details
     }
 
     return render_to_response("Payments/index.html", templateParams, context_instance=RequestContext(request))
+    '''
+    return HttpResponse('Membership of your Company is paid successfully!')
+    #http://127.0.0.1/companies/bbb-862.html
