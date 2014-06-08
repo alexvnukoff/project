eafd34351ebbf385d38314ba9a0ea4b20ebc61cd
Artur K <fatal10110@gmail.com>
2014-06-08 17:46:09 +0300

    payment

--- tppcenter/Payments/urls.py
+++ tppcenter/Payments/urls.py
@@ -10,4 +10,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
      url(r'^$', tppcenter.Payments.views.verify_payment_status, name="verify_payment_status"),
+     url(r'^advertisement/$', tppcenter.Payments.views.pay_for_adv, name="pay_for_adv"),
 )
--- tppcenter/Payments/views.py
+++ tppcenter/Payments/views.py
@@ -35,3 +35,33 @@ def verify_payment_status(request):
             return HttpResponse('False')
     else:
         return HttpResponse('False')
+
+
+def pay_for_adv(request):
+    payment = PayPalPayment()
+    # for production call: payment.verifyAndSave(request, pay_env=1)
+    if payment.verifyAndSave(request):
+        #update company's end_date and paid_till-date
+        item_number = payment.getItemNumber()
+        if len(item_number):
+            if 'Company ID:' in item_number:
+                #get Company ID from string
+                s = [token for token in item_number.split() if token.isdigit()]
+                item_id = s[0]
+                if len(item_id):
+                    try:
+                        comp = Company.objects.get(pk=int(item_id))
+                        new_end_date = comp.paid_till_date + datetime.timedelta(days=365)
+                        comp.end_date = new_end_date
+                        comp.paid_till_date = new_end_date
+                        comp.save()
+                    except:
+                        return HttpResponse('False')
+                else:
+                    return HttpResponse('False')
+            else:
+                return HttpResponse('False')
+        else:
+            return HttpResponse('False')
+    else:
+        return HttpResponse('False')
