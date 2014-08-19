ba25e14ebedb1217e3b83df333c3371fd1449c73
root <root@ip-172-31-16-232.eu-west-1.compute.internal>
2014-08-19 16:01:49 +0000

    sett fix

--- tpp/settings.py
+++ tpp/settings.py
@@ -293,7 +293,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 HAYSTACK_CONNECTIONS = {
     'default':{
         'ENGINE': 'tpp.backend.MultilingualElasticEngine',
-        'URL': '54.77.12.192:9200',
+        'URL': 'ec2-54-77-12-192.eu-west-1.compute.amazonaws.com:9200',
         'INDEX_NAME': 'lang-en',
     },
 }
