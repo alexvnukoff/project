b142d79f5567407c1593ad6c890f8da6331bbc48
root <root@ip-172-31-27-106.eu-west-1.compute.internal>
2014-08-23 17:22:09 +0000

    sett fix

--- tpp/settings.py
+++ tpp/settings.py
@@ -295,7 +295,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 HAYSTACK_CONNECTIONS = {
     'default':{
         'ENGINE': 'tpp.backend.MultilingualElasticEngine',
-        'URL': 'http://ec2-54-72-220-8.eu-west-1.compute.amazonaws.com:8983/solr/sorl-en'
+        'URL': 'http://ec2-54-72-220-8.eu-west-1.compute.amazonaws.com:8983/solr/en'
     },
 }
 
@@ -306,7 +306,7 @@ for lang in LANGUAGES:
 
     HAYSTACK_CONNECTIONS['default' + '_' + lang[0]] = {
         'ENGINE': HAYSTACK_CONNECTIONS['default']['ENGINE'],
-        'URL': 'http://ec2-54-72-220-8.eu-west-1.compute.amazonaws.com:8983/sorl-' + lang[0],
+        'URL': 'http://ec2-54-72-220-8.eu-west-1.compute.amazonaws.com:8983/solr/' + lang[0],
     }
 
 HAYSTACK_SIGNAL_PROCESSOR = 'core.signals.ItemIndexSignal'
