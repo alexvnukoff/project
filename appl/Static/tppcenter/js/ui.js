5dd3dbc7cae6415f22fad50be1eaf6bbfb4b4401
fatal10110 <fatal10110@gmail.com>
2014-08-26 12:21:31 +0300

    Little fixes

--- appl/Static/tppcenter/js/ui.js
+++ appl/Static/tppcenter/js/ui.js
@@ -213,7 +213,9 @@ var ui =
 
     initMenu: function() {
         var pathname = window.location.pathname;
-        pathname = pathname.substring(1, pathname.length - 1).split('/')
+        pathname = pathname.substring(1, pathname.length - 1).split('/');
+
+        var url = '/'
 
         if (pathname.length == 2 && pathname[1] == 'my')
         {
