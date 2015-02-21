8925fafa64615c2b8aa38a09df298e56c9e15df3
Artur <fatal10110@gmail.com>
2015-02-21 20:37:25 +0200

    wsgi settings

--- tpp/usersites_settings.py
+++ tpp/usersites_settings.py
@@ -4,5 +4,5 @@ SITE_ID = None
 ALLOWED_HOSTS = ['*']
 
 WSGI_APPLICATION = 'usersites.wsgi.application'
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, '..', 'usersites', 'templates')]
 ROOT_URLCONF = 'usersites.urls'
