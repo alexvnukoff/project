41c88a8817f4bac220a1b9f44213a10d722bb17c
fatal10110 <fatal10110@gmail.com>
2015-10-02 00:14:49 +0300

    new structure

--- centerpokupok/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from mptt.admin import MPTTModelAdmin
-from centerpokupok.models import B2CProductCategory, B2CProduct
-
-admin.site.register(B2CProductCategory, MPTTModelAdmin)
-admin.site.register(B2CProduct, ModelAdmin)
--- tpp/context_processors.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.sites.shortcuts import get_current_site
-
-
-def site_processor(request):
-    return {'site': get_current_site(request)}
