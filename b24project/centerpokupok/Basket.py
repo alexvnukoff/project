bc9a00873601774fb03642ab7eb90292decaa0b1
Sergey Silaev <hello@ssilaev.com>
2015-12-19 23:40:46 +0300

    additionals

---
+++ b24project/centerpokupok/Basket.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.utils.timezone import now
+from centerpokupok.models import UserBasket, BasketItem
+from django.contrib.sites.shortcuts import get_current_site
+
+
+class ItemAlreadyExists(Exception):
+    pass
+
+class ItemDoesNotExist(Exception):
+    pass
+
+class Basket:
+    def __init__(self, request):
+
+        self.basket_id = request.session['uuid_hash']
+
+        if self.basket_id:
+            try:
+                basket = UserBasket.objects.get(user_uuid=self.basket_id, site_id=get_current_site(request).id, checked_out=False)
+            except UserBasket.DoesNotExist:
+                basket = self.new(request)
+        else:
+            basket = self.new(request)
+        self.basket = basket
+
+    def __iter__(self):
+        for item in self.basket.items.all():
+            yield item
+
+    def new(self, request):
+        basket = UserBasket(created=now())
+        basket.user_uuid = request.session['uuid_hash']
+        basket.site_id = get_current_site(request).id
+        basket.currency = request.session['basket_currency']
+        basket.paypal = request.session['company_paypal']
+        basket.save()
+        return basket
+
+    def add(self, product, quantity=1):
+        try:
+            item = BasketItem.objects.get(basket=self.basket, product=product)
+        except BasketItem.DoesNotExist:
+            item = BasketItem()
+            item.basket = self.basket
+            item.product_id = product
+            item.quantity = quantity
+            item.save()
+        else: #ItemAlreadyExists
+            item.quantity = int(quantity)
+            item.save()
+
+    def remove(self, product):
+        try:
+            item = BasketItem.objects.get(basket=self.basket, product=product)
+        except BasketItem.DoesNotExist:
+            raise ItemDoesNotExist
+        else:
+            item.delete()
+
+    def update(self, product, quantity):
+        try:
+            item = BasketItem.objects.get(basket=self.basket, product=product)
+        except BasketItem.DoesNotExist:
+            raise ItemDoesNotExist
+        else: #ItemAlreadyExists
+            if quantity == 0:
+                item.delete()
+            else:
+                item.quantity = int(quantity)
+                item.save()
+
+    def count(self):
+        result = 0
+        for item in self.basket.items.all().select_related('product'):
+            result += item.quantity
+        return result
+
+    def currency(self):
+        if not self.basket.currency:
+            return False
+        return self.basket.currency
+
+    def paypal(self):
+        if not self.basket.paypal:
+            return False
+        return self.basket.paypal
+
+    def summary(self):
+        result = 0
+        for item in self.basket.items.all().select_related('product'):
+            result += item.product.get_discount_price() * item.quantity
+        return result
+
+    def clear(self):
+        for item in self.basket.items.all():
+            item.delete()
+
