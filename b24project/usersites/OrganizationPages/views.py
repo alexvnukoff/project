59d68e4f252b013f7b776ad8830a95a667b94a6b
kostnikolas <kostnikolas@yandex.ru>
2016-01-14 00:33:38 +0300

    Added user templates

--- b24project/usersites/OrganizationPages/views.py
+++ b24project/usersites/OrganizationPages/views.py
@@ -11,7 +11,7 @@ from b24online.models import AdditionalPage, Department
 from usersites.OrganizationPages.forms import ContactForm
 from usersites.cbv import ItemDetail
 
-from ..mixins import UserTemplateMixin
+from usersites.mixins import UserTemplateMixin
 
 
 class PageDetail(UserTemplateMixin, ItemDetail):
--- b24project/usersites/Proposals/views.py
+++ b24project/usersites/Proposals/views.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext as _
 from b24online.models import BusinessProposal
 
 from usersites.cbv import ItemList, ItemDetail
-from ..mixins import UserTemplateMixin
+from usersites.mixins import UserTemplateMixin
 
 class BusinessProposalList(UserTemplateMixin, ItemList):
     model = BusinessProposal
