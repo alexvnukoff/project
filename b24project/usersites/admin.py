6737858f672bb4b410ff1187a935703b828236ca
Sergey Silaev <hello@ssilaev.com>
2016-01-15 01:53:35 +0300

    image upload @receiver

--- b24project/usersites/admin.py
+++ b24project/usersites/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from usersites.models import ExternalSiteTemplate, UserSite, UserSiteTemplate
 
+class UserSiteTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'folder_name',)
+
 admin.site.register(ExternalSiteTemplate, ModelAdmin)
 admin.site.register(UserSite, ModelAdmin)
-admin.site.register(UserSiteTemplate, ModelAdmin)
+admin.site.register(UserSiteTemplate, UserSiteTemplateAdmin)
