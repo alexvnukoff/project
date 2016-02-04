67dcb0b9b6b781da3cc6c6e1f4cc764e7a194e1c
fatal10110 <fatal10110@gmail.com>
2016-02-04 21:32:04 +0200

    some fixes

--- b24project/appl/realtime.py
+++ b24project/appl/realtime.py
@@ -5,8 +5,6 @@ from sockjs.tornado import SockJSConnection
 import django
 from django.conf import settings
 import json
-from appl.models import *
-
 
 # start of kmike's sources
 _engine = import_module(settings.SESSION_ENGINE)
@@ -15,8 +13,10 @@ _engine = import_module(settings.SESSION_ENGINE)
 def get_session(session_key):
     return _engine.SessionStore(session_key)
 
+
 def get_current_company(session):
     return session._session.get('current_company', False)
+
 
 def get_user(session):
     class Dummy(object):
@@ -25,6 +25,8 @@ def get_user(session):
     django_request = Dummy()
     django_request.session = session
     return django.contrib.auth.get_user(django_request)
+
+
 # end of kmike's sources
 
 
@@ -46,11 +48,11 @@ class Connection(SockJSConnection):
             Вешаем подписчиков на каналы сообщений.
         """
         self.redis_client = tornadoredis.Client(
-                host=ORDERS_REDIS_HOST,
-                port=ORDERS_REDIS_PORT,
-                password=ORDERS_REDIS_PASSWORD,
-                selected_db=ORDERS_REDIS_DB
-            )
+            host=ORDERS_REDIS_HOST,
+            port=ORDERS_REDIS_PORT,
+            password=ORDERS_REDIS_PASSWORD,
+            selected_db=ORDERS_REDIS_DB
+        )
         self.redis_client.connect()
 
         yield tornado.gen.Task(self.redis_client.subscribe, [
@@ -59,16 +61,16 @@ class Connection(SockJSConnection):
             'partner'
         ])
         self.redis_client.listen(self.on_redis_queue)  # при получении сообщения
-                           #  вызываем self.on_redis_queue
+        #  вызываем self.on_redis_queue
 
     def send(self, msg_type, message):
         """
         Оправка сообщений.
         """
         return super(Connection, self).send(json.dumps({
-                'type': msg_type,
-                'data': message,
-            }))
+            'type': msg_type,
+            'data': message,
+        }))
 
     def on_open(self, request):
         """
@@ -76,8 +78,7 @@ class Connection(SockJSConnection):
         """
         self.django_session = get_session(request.get_cookie('sessionid').value)
         self.user = get_user(self.django_session)
-        #self.current_company = get_current_company(self.django_session)
-
+        # self.current_company = get_current_company(self.django_session)
 
     def on_message(self, msg):
         """
@@ -90,25 +91,23 @@ class Connection(SockJSConnection):
         Обновление в списке заказов
         """
         if message.kind == 'message':  # сообщения у редиса бывают разного типа, 
-                           # много сервисных, нам нужны только эти
+            # много сервисных, нам нужны только эти
             message_body = json.loads(message.body)  # разворачиваем сабж, как вы
-                                   #  поняли я передаю данные в JSON
+            #  поняли я передаю данные в JSON
 
             # в зависимости от канала получения распределяем сообщения
             if message.channel == 'notification' and message_body.get('user', False) == self.user.pk:
-                self.sendNoification(message, message_body)
+                self.send_notification(message, message_body)
 
-            #send message to recipient
+            # send message to recipient
             if message.channel == 'private_massage':
 
                 recipient = message_body.get('recipient', False)
 
+                if int(recipient) == self.user.pk:
+                    self.send_notification(message, message_body)
 
-
-                if Cabinet.objects.get(pk=int(recipient)).user.pk == self.user.pk:
-                    self.sendNoification(message, message_body)
-
-    def sendNoification(self, message, message_body):
+    def send_notification(self, message, message_body):
         self.send(message.channel, message_body)
 
     def on_close(self):
@@ -116,8 +115,8 @@ class Connection(SockJSConnection):
         При закрытии соединения отписываемся от сообщений
         """
         self.redis_client.unsubscribe([
-           'notification',
-           'private_massage',
-           'partner'
+            'notification',
+            'private_massage',
+            'partner'
         ])
         self.redis_client.disconnect()
