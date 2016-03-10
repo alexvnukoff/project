883eb280e8e5a646f98afc9787967ae4425ddc4d
Sergey Silaev <silaev.sergey@gmail.com>
2016-03-10 14:31:09 +0300

    usersites additionals

--- b24project/usersites/B2CProducts/views.py
+++ b24project/usersites/B2CProducts/views.py
@@ -6,7 +6,8 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import loader, RequestContext
 from django.template import loader
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
@@ -14,7 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from django.views.generic.edit import FormView
 from paypal.standard.forms import PayPalPaymentsForm
-
+from b24online.utils import get_template_with_base_path
 from centerpokupok.Basket import Basket
 from centerpokupok.forms import OrderEmailForm
 from centerpokupok.models import B2CProduct, B2CProductCategory
@@ -147,7 +148,7 @@ class B2CProductDetail(UserTemplateMixin, ItemDetail):
 
 
 class B2CProductBasket(View):
-
+    template_name = 'B2CProducts/basket.html'
     def get(self, request):
         basket      = Basket(request)
         del_product = request.GET.get('del')
@@ -188,7 +189,12 @@ class B2CProductBasket(View):
                 return HttpResponseRedirect((reverse('b2c_products:basket')))
             return HttpResponseNotFound()
 
-        return render(request, 'usersites/B2CProducts/basket.html', data)
+        return render_to_response(
+                 get_template_with_base_path(self.template_name),
+                 data,
+                 context_instance=RequestContext(request)
+                 )
+
 
     def post(self, request):
         basket     = Basket(request)
--- b24project/usersites/OrganizationPages/urls.py
+++ b24project/usersites/OrganizationPages/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from usersites.OrganizationPages.views import Contacts, PageDetail, Structure
+from usersites.OrganizationPages.views import (Contacts, PageDetail,
+        Structure, About, Gallery)
 
 urlpatterns = [
-    url(r'^about$', TemplateView.as_view(template_name='usersites/OrganizationPages/about.html'), name='about'),
-    url(r'^structure$', Structure.as_view(), name='structure'),
+    url(r'^about/$', About.as_view(), name='about'),
+    url(r'^structure/$', Structure.as_view(), name='structure'),
     url(r'^contacts/$', Contacts.as_view(), name='contacts'),
-    url(r'^gallery/$', TemplateView.as_view(template_name='usersites/OrganizationPages/gallery.html'), name='gallery'),
+    url(r'^gallery/$', Gallery.as_view(), name='gallery'),
     url(r'^(?P<slug>[a-zA-z0-9-]+)-(?P<pk>[0-9]+)\.html$', PageDetail.as_view(), name='detail'),
 ]
