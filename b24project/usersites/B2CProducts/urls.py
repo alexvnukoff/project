a83d15673b7fa2ed06d10f903fb01f25a5afb48d
Sergey Silaev <silaev.sergey@gmail.com>
2016-03-15 21:39:26 +0300

    templates, refactoring, merge

--- b24project/usersites/B2CProducts/urls.py
+++ b24project/usersites/B2CProducts/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.utils.translation import ugettext as _
 
-from usersites.B2CProducts.views import B2CProductDetail, B2CProductBasket, \
-    B2CProductByEmail
+from usersites.B2CProducts.views import (B2CProductDetail, B2CProductBasket,
+    B2CProductByEmail)
 from usersites.views import render_page
 
 urlpatterns = [
