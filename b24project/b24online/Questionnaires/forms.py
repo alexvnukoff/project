934f5d36e4129e478ac3032146309f3b3cc095d5
Andrey Kozhevnikov <anarko7@gmail.com>
2016-03-15 08:27:38 +0300

    refs $843 - add forms and views for questionnaire

--- b24project/b24online/Questionnaires/forms.py
+++ b24project/b24online/Questionnaires/forms.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext as _
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
+from b24online import InvalidParametersError
 from b24online.models import (Questionnaire, Question, Answer, B2BProduct)
 from centerpokupok.models import B2CProduct
 
@@ -16,44 +17,38 @@ logger = logging.getLogger(__name__)
 
 class QuestionnaireForm(forms.ModelForm):
 
-    content_type_label = _('Object type for questionnaire')
-    object_id_label = _('Object for questionnaire')
+    item_label = _('Questionnaire for')
 
     class Meta:
         model = Questionnaire
-        fields = ['content_type', 'object_id', 'name', 'short_description', 
-                  'description', 'image']
+        fields = ['name', 'short_description', 'description', 'image']
 
-    def __init__(self, request, content_type_id=None, instance_id=None, 
+    def __init__(self, request, content_type_id=None, item_id=None, 
                  *args, **kwargs):
         cls = type(self)
         super(QuestionnaireForm, self).__init__(*args, **kwargs)
         self.request = request
-        self._object = None
-        self._content_type = None
-        if content_type_id:
+        try:
+            self._content_type = ContentType.objects.get(
+                pk=content_type_id
+            )
+        except ContentType.DoesNotExist:
+            raise InvalidParametersError(
+                _('Invalid ContentType ID')
+            )
+        else:
+            model_class = self._content_type.model_class()
             try:
-                self._content_type = ContentType.objects.get(
-                    pk=content_type_id
+                self.item = model_class.objects.get(pk=item_id)
+            except model_class.DoesNotExist:
+                raise InvalidParametersError(
+                    _('Invalid Object ID')
                 )
-            except ContentType.DoesNotExist:
-                pass
-            else:
-                self.fields = ['object_id', 'name', 'short_description', 
-                  'description', 'image']
-                model_class = self._content_type.model_class()
-                if instance_id:
-                    try:
-                        self._object = model_class.objects.get(pk=instance_id)
-                    except model_class.DoesNotExist:
-                        pass
-                    else:
-                        self.fields = ['name', 'short_description', 
-                          'description', 'image']
 
-        if 'content_type' in self.fields:
-            self.fields['content_type'].label = cls.content_type_label
-            self.fields['content_type'].required = True
-
-        if 'object_id':
-            self.fields['object_id'].label =cls.object_id_label
+    def save(self, commit=True):
+        instance = super(QuestionnaireForm, self).save(commit=False)
+        instance.content_type = self._content_type
+        instance.item = self.item
+        if commit:
+            instance.save()
+        return instance
--- b24project/b24online/Questionnaires/views.py
+++ b24project/b24online/Questionnaires/views.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+import logging
+
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
@@ -14,6 +16,8 @@ from b24online.cbv import (ItemsList, ItemDetail, ItemUpdate, ItemCreate,
                            ItemDeactivate)
 from b24online.Questionnaires.forms import QuestionnaireForm
 
+logger = logging.getLogger(__name__)
+
 
 class QuestionnaireCreate(ItemCreate):
     model = Questionnaire
@@ -21,24 +25,33 @@ class QuestionnaireCreate(ItemCreate):
     template_name = 'b24online/Questionnaires/addForm.html'
     success_url = reverse_lazy('questionnaires:main')
 
-    def dispatch(self, request, *args, **kwargs):
-        self.object = None
-        form_class = self.get_form_class()
-        return super(QuestionnaireCreate, self)\
-            .dispatch(request, *args, **kwargs)
-        
     def get(self, request, *args, **kwargs):
-        form = self.form_class(request)
+        self.object = None
+        content_type_id = kwargs.pop('content_type_id')
+        item_id = kwargs.pop('item_id')
+        form = self.form_class(
+            request, 
+            content_type_id=content_type_id,
+            item_id=item_id,
+        )
         return self.render_to_response(
             self.get_context_data(form=form)
         )
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request, instance=self.object, 
-                               data=request.POST)
+        self.object = None
+        content_type_id = kwargs.pop('content_type_id')
+        item_id = kwargs.pop('item_id')
+        form = self.form_class(
+            request, 
+            content_type_id=content_type_id,
+            item_id=item_id,
+            data=request.POST,
+            files=request.FILES
+        )
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(self.get_success_url())
+            return HttpResponseRedirect(self.success_url)
         return self.render_to_response(self.get_context_data(form=form))
 
 
