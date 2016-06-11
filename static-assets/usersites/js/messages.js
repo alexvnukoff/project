f32fefd0af62d4b494ffe62dce70b4f11be79ffa
Andrey Kozhevnikov <anarko7@gmail.com>
2016-06-11 15:38:48 +0300

    refs #925 - add the chats

--- static-assets/usersites/js/messages.js
+++ static-assets/usersites/js/messages.js
@@ -17,20 +17,11 @@ $(function() {
             success: function(data) {
                 if (data.code == 'error') {
                     $.each(data.errors, function(field_name, field_errors) {
-                        /**
-                         * Обработка ошибок формы
-                         * index - имя поля, item - сообщение,
-                         * необходимо:
-                         *  - изменить тип поля, и приглашения;
-                         *  - заполнить блок текстом сообщения;
-                         *  - сделать блок видимым. 
-                         */
-                    
-                        var fieldId = '#id_' + field_name, // ИД поля
-                            fieldErrorId = fieldId + '_errors', // ИД блока для ошибки
-                            field = $(fieldId), // Поле
-                            fieldError = $(fieldErrorId), // Блок для ошибки
-                            fieldParentDiv = $(field).parent(); // Родительский блок для поля
+                        var fieldId = '#id_' + field_name,
+                            fieldErrorId = fieldId + '_errors',
+                            field = $(fieldId),
+                            fieldError = $(fieldErrorId),
+                            fieldParentDiv = $(field).parent();
                         
                         $(fieldError).html(field_errors); 
                         $(fieldError).removeClass('error-hidden');
@@ -43,6 +34,64 @@ $(function() {
         });
         return false;
 	});
+	
+	// Список чатов и его обработка
+	var chats = $('#chats');
+	if (chats.length) {
+    	var chatsUI = new ChatsUI(chats);
+    	chatsUI.init();
+    }
 });
 
- 
+function _id(id_str) {
+    return (typeof id_str == 'string') ? '#' + id_str : '';
+}
+
+function _cls(cls_str) {
+    return (typeof cls_str == 'string') ? '.' + cls_str : '';
+}
+
+function ChatsUI(chats) {
+    this.chatsListID = 'chats_list';
+    this.messagesListIO = 'messages_list';
+    this.itemCls = 'list-group-item';
+    this.activeItemCls = 'active-item';
+    this.currentItem = null;
+    this.selectItemCls = 'li.' + this.itemCls;
+} 
+
+ChatsUI.prototype.init = function() {
+    var self = this;    
+    self.chatsList = $(_id(self.chatsListID));
+    if (!self.currentItem) {
+        firstItem = $(self.chatsList).find(_cls(self.itemCls) + ':first');
+        if (firstItem.length > 0) {
+            self.activateItem(firstItem);
+            self.drawMessages(self.currentItem);
+        }
+    }
+    $(self.selectItemCls).on('click', function(event) {
+        self.onSelectItem(this);
+    });
+}
+
+
+ChatsUI.prototype.activateItem = function(item) {
+    var self = this;
+    var prevItem = $(self.chatsList).find(_cls(self.activeItemCls));
+    if (prevItem.length) {
+        $(prevItem).removeClass(self.activeItemCls);
+    }
+    self.currentItem = item;
+    $(self.currentItem).addClass(self.activeItemCls);
+}
+
+ChatsUI.prototype.onSelectItem = function(item) {
+    var self = this;
+    self.activateItem(item);
+}
+
+ChatsUI.prototype.drawMessages = function(currentItem) {
+    
+}
+
