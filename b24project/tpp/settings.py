e3d1fd9b6ba774fbf03b74e0e5cda162abd2f5d6
Sergey Silaev <silaev.sergey@gmail.com>
2016-07-12 18:52:52 +0300

    Add django-compressor, csscompress, jsMin(embeded), Update requirements.txt

--- b24project/tpp/settings.py
+++ b24project/tpp/settings.py
@@ -127,7 +127,8 @@ EXTERNAL_APPS = (
     'guardian',
     'captcha',
     'paypal.standard.ipn',
-    'rest_framework'
+    'rest_framework',
+    'compressor'
 )
 
 # the order is important!
@@ -176,6 +177,12 @@ MIDDLEWARE_CLASSES = (
     'centerpokupok.BasketMiddleware.Basket',
     'b24online.stats.middleware.RegisteredEventMiddleware',
     'tpp.GeolocationFilterByRegion.GeolocationMiddleware',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
