70405fa72ce364becf09e30be3cfffa6769780e6
Andrey Kozhevnikov <anarko7@gmail.com>
2016-07-17 10:58:33 +0300

    refs #1008 - show the right original image

--- b24project/usersites/forms.py
+++ b24project/usersites/forms.py
@@ -7,6 +7,7 @@ from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext as _
 
+from b24online.models import Profile
 from usersites.models import UserSiteTemplate
 from b24online.utils import handle_uploaded_file
 
@@ -121,3 +122,32 @@ def create_extra_form(instance, request):
         except ExtraParamsError:
             pass
     return None
+
+
+class ProfileForm(forms.ModelForm):
+    birthday = forms.DateField(input_formats=["%d/%m/%Y"])
+
+    def __init__(self, *args, **kwargs):
+        # first call parent's constructor
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk and self.instance.birthday:
+            self.initial['birthday'] = self.instance.birthday.strftime('%d/%m/%Y')
+
+        self.fields['first_name'].widget.attrs.update({'class': 'text'})
+        self.fields['middle_name'].widget.attrs.update({'class': 'text'})
+        self.fields['last_name'].widget.attrs.update({'class': 'text'})
+        self.fields['mobile_number'].widget.attrs.update({'class': 'text'})
+        self.fields['site'].widget.attrs.update({'class': 'text'})
+        self.fields['profession'].widget.attrs.update({'class': 'text'})
+        self.fields['birthday'].widget.attrs.update({'class': 'date'})
+
+    class Meta:
+        model = Profile
+        fields = ('country', 'first_name', 'middle_name', 'last_name', 'avatar', 'mobile_number',
+                  'site', 'profession', 'sex', 'user_type')
+        widgets = {
+            'sex': forms.RadioSelect,
+            'user_type': forms.RadioSelect
+        }
+
