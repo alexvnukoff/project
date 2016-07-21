dca80d2382cf1b992e5d2e47144494d204bdb13f
Sergey Silaev <silaev.sergey@gmail.com>
2016-07-21 23:51:02 +0300

    LeadsStore v.0.8

--- b24project/b24online/Leads/urls.py
+++ b24project/b24online/Leads/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from b24online.Leads.views import IndexLeadsList, GoLeadDelete
+from b24online.Leads.views import IndexLeadsList, GoLeadDelete, GoLeadUpdate
 
 urlpatterns = [
     url(r'^$', IndexLeadsList.as_view(my=True), name='main'),
     url(r'^my/page(?P<page>[0-9]+)?/$', IndexLeadsList.as_view(my=True), name="main_paginator"),
-    #url(r'^update/(?P<pk>[0-9]+)/$', GoLeadUpdate.as_view(), name="update"),
+    url(r'^update/(?P<pk>[0-9]+)/$', GoLeadUpdate.as_view(), name="update"),
     url(r'^delete/(?P<pk>[0-9]+)/$', GoLeadDelete.as_view(), name="delete"),
 ]
--- b24project/b24online/Leads/views.py
+++ b24project/b24online/Leads/views.py
@@ -10,6 +10,7 @@ from guardian.mixins import LoginRequiredMixin
 from b24online.models import Branch, Organization, LeadsStore
 from b24online.cbv import ItemsList, ItemUpdate
 from django.views.generic import UpdateView, DeleteView
+from b24online.Leads.forms import LeadEditForm
 
 
 class IndexLeadsList(LoginRequiredMixin, ItemsList):
@@ -49,10 +50,8 @@ class GoLeadDelete(DeleteView):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
-
         if not obj.has_perm(request.user):
             return HttpResponseRedirect(reverse('denied'))
-
         return super().dispatch(request, *args, **kwargs)
 
     success_url = reverse_lazy('leads:main')
@@ -63,4 +62,32 @@ class GoLeadDelete(DeleteView):
         success_url = self.get_success_url()
         self.object.delete()
         return HttpResponseRedirect(success_url)
+
+
+class GoLeadUpdate(ItemUpdate):
+    model = LeadsStore
+    form_class = LeadEditForm
+    template_name = 'b24online/Leads/addForm.html'
+    success_url = reverse_lazy('leads:main')
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not obj.has_perm(request.user):
+            return HttpResponseRedirect(reverse('denied'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form(self.get_form_class())
+        return self.render_to_response(self.get_context_data(form=form))
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self):
+        return self.render_to_response(self.get_context_data(form=form))
 
