c731a79d42dc4335fb884f84b1084547ffd350f4
Andrey Kozhevnikov <anarko7@gmail.com>
2016-07-27 13:06:25 +0300

    refs #1017 - the golan templates

--- static-assets/usersites/js/delivery.js
+++ static-assets/usersites/js/delivery.js
@@ -3,10 +3,38 @@
  */
 
 $(function() {
-    var deliveryDataForm = $('#delivery_form'), 
+
+    // Для корзины
+    var DELIVERY_URL = '/b2c-products/delivery.html';
+    var orderByEmailButton = $('#order_by_email'),
         paypalForm = $('#paypal_form_layer > form'),
-        paypalFormButton = $(paypalForm).find('input[type="image"]:first');
-    if (paypalFormButton.length > 0) {
+        paypalFormButton = $(paypalForm).find('input[type="image"]:first'),
+        deliveryDataForm = $('#delivery_form');
+        
+    if (orderByEmailButton.length > 0 && paypalForm.length > 0) {    
+        $(orderByEmailButton).on('click', function(event) {
+            event.preventDefault();
+            var url = $(this).attr('href');
+            if ($('#need_delivery').is(":checked")) {
+                url += '?need_delivery=true';
+            }
+            window.location.href = url;
+            return false;
+        });
+    
+        // Для кнопки-рисунка отправки информации на PayPal
+        if (paypalFormButton.length > 0) {
+            $(paypalFormButton).on('click', function(event) {
+                event.preventDefault();
+                if ($('#need_delivery').is(":checked")) {
+                    window.location.href = DELIVERY_URL;
+                } else {
+                    $(paypalForm).submit();
+                }
+                return false;
+            });        
+        }
+    } else if (deliveryDataForm.length > 0 && paypalForm.length > 0) {
         $(paypalFormButton).on('click', function(event) {
             event.preventDefault();
             $('.form_field_error').each(function() {
