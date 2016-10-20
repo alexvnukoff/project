024064f656e11efab259ab9021904f0b1a293fdc
Sergey Silaev <ssilaev@protonmail.ch>
2016-10-20 16:09:51 +0300

    jquery ui calendar css fixes

--- static-assets/b24online/js/deals.js
+++ static-assets/b24online/js/deals.js
@@ -6,28 +6,28 @@ $(document).ready(
     function () {
 	$('.toggle-next').not('.force-open').each(
 	    function () {
-		$(this).next().hide();		    
+		$(this).next().hide();
 	    });
 	$('.toggle-next.force-open').addClass('toggled');
 	$('.toggle-next').click(
 	    function () {
-    		$(this).toggleClass('toggled').next().toggle();	
+    		$(this).toggleClass('toggled').next().toggle();
 	});
 
     $('.toggle-child').not('.force-open').each(function () {
         var child_class = '.' + $(this).attr('id') + '-child';
-      	$(this).nextAll(child_class).hide();		    
+      	$(this).nextAll(child_class).hide();
     });
     $('.toggle-child.force-open').addClass('toggled');
   	$('.toggle-child').click(function () {
         var child_class = '.' + $(this).attr('id') + '-child';
-        $(this).toggleClass('toggled').nextAll(child_class).toggle();	
+        $(this).toggleClass('toggled').nextAll(child_class).toggle();
     });
 
-	$( ".date" ).datepicker({  
-        changeMonth: true,   
-        changeYear: true,  
-        yearRange: "2010:",   
+	$( ".date" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "2010:",
         dateFormat: "dd/mm/yy"
     });
 });
