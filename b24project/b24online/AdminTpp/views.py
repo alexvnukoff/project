071c57b348e079225b08e4f2f38c49b2ab319233
Sergey Silaev <ssilaev@protonmail.ch>
2016-11-08 21:24:32 +0300

    Add utf-8 encoding

--- b24project/b24online/AdminTpp/views.py
+++ b24project/b24online/AdminTpp/views.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import json
 
 from django.contrib.auth import get_user_model
--- b24project/b24online/Tpp/views.py
+++ b24project/b24online/Tpp/views.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
