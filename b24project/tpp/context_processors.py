9c4aba80b6b003f3c962ed4a116d6e2624b4a9f3
Sergey Silaev <ssilaev@protonmail.ch>
2016-12-09 00:08:22 +0300

    site_languages v.1.0

--- b24project/tpp/context_processors.py
+++ b24project/tpp/context_processors.py
@@ -1,5 +1,6 @@
 from b24online.utils import get_current_organization
 from tpp.DynamicSiteMiddleware import get_current_site
+from django.conf import settings
 
 
 def site_processor(request):
@@ -9,4 +10,25 @@ def site_processor(request):
 def current_organization_processor(request):
     return {
         'current_organization': get_current_organization(request)
+    }
+
+
+def site_languages_processor(request):
+    LANGUAGES = settings.LANGUAGES
+
+    try:
+        site_languages = get_current_site().user_site.languages
+    except:
+        site_languages = None
+
+    if site_languages:
+        obj = []
+        for code, lang in LANGUAGES:
+            if code in site_languages:
+                obj.append((code, lang))
+    else:
+        obj =  LANGUAGES
+
+    return {
+        'get_site_languages': obj
     }
